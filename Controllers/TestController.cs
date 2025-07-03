a15a6f2e496cb4cf6e1f10a1150945595ee4d945
Hoang long Nguyen <longasasa@gmail.com>
2025-07-03 15:01:29 +0700

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Test Name",
+                DateofBirth = new DateTime(1990, 1, 1)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ Models/AnotherTestModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Models
+{
+    public class AnotherTestModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
